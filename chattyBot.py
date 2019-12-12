cc1faf9b6985a6bd4d7505699ad042372af685a3
xlinx <xxlinxx@gmail.com>
2019-12-12 17:57:28 +0800

    start a chat between bot1 and bot2.

--- chattyBot.py
+++ chattyBot.py
@@ -1,22 +1,11 @@
-# This is a voice-output interface for CleverBot, a chatbot.
-# Needs python3, cleverwrap and flite chattyto run
-# python should be present in a unix terminal
-#
-# pip install cleverwrap
-# sudo apt-get install flite
-#
-# Usage:
-# python chatBot.py
-# then just type after the > prompt
-# to leave, type > I'm leaving
-
+from cleverwrap import CleverWrap
 from googletrans import Translator
 translator = Translator()
-from cleverwrap import CleverWrap
-chatBot = CleverWrap("CC6zkRgO9TPlPcz6AO_zLCunNBg")
+chatBot1 = CleverWrap("CC6zkRgO9TPlPcz6AO_zLCunNBg")
+chatBot2 = CleverWrap("CC6zkRgO9TPlPcz6AO_zLCunNBg")
 import subprocess
 import sys
-#hihi
+
 shell = True
 #chatBot.reset()
 
@@ -25,19 +14,27 @@ def speak(this):
     subprocess.run(['say', str(this)])
 
 speak('I\'m listening')
-var_CH = input('> ')
-var_EN=translator.translate(var_CH, src='zh-tw',dest='en')
-
+var1_CH = input('> ')
+var1_EN=translator.translate(var1_CH,dest='en', src='auto')
+print (var1_CH,' = ',var1_EN.text) 
 chatting = True
 while chatting == True:
-        if(var_EN == 'I\'m leaving'):
+        if(var1_EN == 'I\'m leaving'):
             chatting = False
             break
-        
-        reply = chatBot.say(str(var_EN.text))
-        print('var_EN=',var_EN.text,'var_CH',var_CH)
-        
-        speak(reply)
-        var = input('> ')
+        reply1_EN = chatBot1.say(str(var1_EN))
+        reply1_CH=translator.translate(reply1_EN,dest='zh-tw', src='en')
+        # reply1_CH=reply1_CH.split("=", 1)
+        print (reply1_CH.text,' = ',reply1_EN) 
+        subprocess.run(['say', str(reply1_CH.text)])
+        # speak(reply1_CH)
+        reply2_EN = chatBot2.say(str(reply1_EN))
+        reply2_CH=translator.translate(reply2_EN,dest='zh-tw', src='en')
+        # reply2_CH=reply2_CH.split("=", 1)
+        print (reply2_CH.text,' = ',reply2_EN) 
+        subprocess.run(['say', str(reply2_CH.text)])
+        # speak(reply2_CH)
+
+        var1_EN = reply2_EN
 
 speak("Fine, leave. See if I care.")
